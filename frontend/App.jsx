e4ea8f4fb847e0ef21543e10390de84b6d414a36
HarishE34 <145241212+HarishE34@users.noreply.github.com>
2025-12-12 10:35:29 +0530

    by harish

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,13 +3,15 @@ import { View } from 'react-native'
 import TrackBusScreen from './Components/Passenger/TrackBus'
 import LiveMapPage from './Components/Passenger/Tracking'
 import NotificationsPage from './Components/Passenger/Notification'
+import RewardProgressPage from './Components/Driver/Reward'
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
       {/* <TrackBusScreen /> */}
       {/* <LiveMapPage/> */}
-      <NotificationsPage/>
+      {/* <NotificationsPage/> */}
+      <RewardProgressPage/>
     </View>
   )
 }
