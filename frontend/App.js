25a164516ece6a0cb7f00103fc952c44509a5118
Gokul999coder <145419194+Gokul999coder@users.noreply.github.com>
2025-12-23 20:44:58 +0530

    Delete App.js file and its contentsk
    Removed the entire App.js file, which included imports and the main App component.

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,1 @@
-// App.js
-import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import TrackBusScreen from "./Components/Passenger/TrackBusScreen";
-import SOSScreen from "./Components/Passenger/SOSScreen";
-import DriverDashboard from "./Components/Driver/DriverDashboard";
 
-export default function App() {
-  return (
-    <SafeAreaProvider>
-      <DriverDashboard/>
-    </SafeAreaProvider>
-  );
-}
