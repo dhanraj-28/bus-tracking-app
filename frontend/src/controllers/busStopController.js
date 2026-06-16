8c9cdf97e3c1bba056eb1c34223095899b13b99f
Gokul999coder <itsgokul555@gmail.com>
2026-06-16 14:22:51 +0530

    minor change

--- frontend/src/controllers/busStopController.js
+++ frontend/src/controllers/busStopController.js
@@ -1,54 +1,25 @@
-const busStopService =
-require("../services/busStopService");
+const busStopService = require("../services/busStopService");
 
-const getNearbyBusStops =
-async (req, res) => {
+const getNearbyBusStops = async (req, res) => {
+  try {
+    const { latitude, longitude } = req.query;
 
-try {
+    if (!latitude || !longitude) {
+      return res.status(400).json({
+        message: "Latitude and Longitude required",
+      });
+    }
 
-const {
-latitude,
-longitude,
-} =
-req.query;
+    const stops = await busStopService.fetchNearbyBusStops(latitude, longitude);
 
-if(
-!latitude ||
-!longitude
-){
+    return res.status(200).json(stops);
+  } catch (error) {
+    console.log("CONTROLLER ERROR", error);
 
-return res.status(400).json({
-message:
-"Latitude and Longitude required",
-});
-
-}
-
-const stops =
-await busStopService
-.fetchNearbyBusStops(
-latitude,
-longitude
-);
-
-return res.status(200).json(
-stops
-);
-
-}
-catch(error){
-
-console.log(
-"CONTROLLER ERROR",
-error
-);
-
-return res.status(500).json([]);
-
-}
-
+    return res.status(500).json([]);
+  }
 };
 
 module.exports = {
-getNearbyBusStops,
+  getNearbyBusStops,
 };
